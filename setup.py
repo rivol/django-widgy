b1e77c3d9dc125f1d84c2e0abcbb46078a9804e4
Rivo Laks <rivolaks@gmail.com>
2014-11-18 16:40:33 +0200

    Fix get_version() on Python 3

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ def get_version():
     elif stage == 'alpha':
         process = subprocess.Popen('git rev-parse HEAD'.split(), stdout=subprocess.PIPE)
         stdout, stderr = process.communicate()
-        return number + '-' + stdout.strip()[:8]
+        return number + '-' + stdout.decode('ascii').strip()[:8]
 
 setup(
     name='django-widgy',
